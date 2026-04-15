93b196d2860335795f607a3d580e72f6d7d68714
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-04-15 10:48:07 -0500

    se agregan avances en archivo app.js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const hamburguesa = [
+    {
+        id: 1,
+        nombre: "La Colombiana",
+        precio: 25000,
+        img: "",
+        descripcion: "La hamburguesa que sabe a Colombia Jugosa carne Angus a la parrilla, acompañada de dulce plátano maduro, lechuga fresca, tomate y cebolla, "
+
+    },
+    {
+        id: 2,
+        nombre: "La Mexicana",
+        precio: 25000,
+        img: "",
+        descripcion: "150 gr de res ,La hamburguesa que sabe a Colombia Jugosa carne Angus a la parrilla, acompañada de dulce plátano maduro, lechuga fresca, tomate y cebolla, "
+    },
+    {
+        id: 3,
+        nombre: "Crispy Burger",
+        precio: 25000,
+        img: "",
+        descripcion: "Hamburguesa con 120 gr de carne 100% de res, queso cheddar, cebollas crocantes y salsas de la "
+    },
+    {
+        id: 4,
+        nombre: "La Mexicana",
+        precio: 25000,
+        img: "",
+        descripcion: "150 gr de res ,La hamburguesa que sabe a Colombia Jugosa carne Angus a la parrilla, acompañada de dulce plátano maduro, lechuga fresca, tomate y cebolla, "
+    }
+]
