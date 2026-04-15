7ec6693641d5849860f59d7958204b44730aec23
fasalpa <psfr97@gmail.com>
2026-04-15 10:47:56 -0500

    Llamar los ids al JS

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let navbar = document.getElementById("navbar");
+let cardProducto = document.getElementById("cardProducto");
+let imagenCard = document.getElementById("imagenCard");
+let descripcionProducto = document.getElementById("descripcionProducto");
+let titulo = document.getElementById("descripcionProducto");
+let descripcionProducto = document.getElementById("descripcionProducto");
+
